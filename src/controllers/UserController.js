8df7f5290c924d063e1db4b55715c3bfdbb76fd5
samirvithlani <samir.vithlani83955@gmail.com>
2026-06-21 13:56:44 +0530

    findbyid

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,16 +1,35 @@
 //functions
-const userSchema  = require("../models/UserModel")
+const userSchema = require("../models/UserModel");
 //userScham == db.users
-const getUsers = async(req,res)=>{
+const getUsers = async (req, res) => {
+  //db-->
+  const users = await userSchema.find(); //[]
+  res.json({
+    message: "user api called..",
+    data: users,
+  });
+};
+const getUserById = async (req, res) => {
+  //id -->params.
+  const id = req.params.id;
+  console.log(id);
+  //const foundUser = await userSchema.find({_id:ObjectId("")}) // [{}]
 
-    //db-->
-    const users = await userSchema.find() //[]
+  const foundUser = await userSchema.findById(id); //{}
+  if (foundUser) {
     res.json({
-        message:"user api called..",
-        data:users
-    })
-}
+      message: "user found",
+      data: foundUser,
+    });
+  }
+  else{
+    res.json({
+      message: "user not found",
+    });
+  }
+};
 
 module.exports = {
-    getUsers
-}
+  getUsers,
+  getUserById,
+};
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -2,4 +2,5 @@ const router = require("express").Router()
 const userController = require("../controllers/UserController")
 //router.get("/users",(req,res)=>{}) <<-- (req,res) -->controller
 router.get("/users",userController.getUsers)
+router.get("/user/:id",userController.getUserById)
 module.exports = router
